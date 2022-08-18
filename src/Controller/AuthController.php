d00a85cd30f7b800303c30f3ffcfd65531c7aeae
BsUrbk <bartosz.urbk@gmail.com>
2022-08-18 11:19:47 +0200

    add auth

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -13,19 +13,9 @@ class AuthController extends AbstractController{
     public function login(Request $req): Response{
         if($req->cookies->all('BEARER')){
             return $this->json(['message' => 'You are already logged in']);
-        }else{
-            JWTauth::issueJWT($req->username);
         }
-        
-        
-        return $this->json([
-            'message' => 'Hi',
-        ]);
-    }
 
-    #[Route('/register', name: 'root', methods:['POST','HEAD'])]
-    public function register(): Response{
-        $jwt = JWTauth::issueJWT('dud');
+        JWTauth::issueJWT($req->username);
         
         return $this->json([
             'message' => 'Hi',
@@ -34,7 +24,7 @@ class AuthController extends AbstractController{
 
     #[Route('/reset', name: 'root', methods:['POST','HEAD'])]
     public function reset(): Response{
-        $jwt = JWTauth::issueJWT('dud');
+        JWTauth::issueJWT('dud');
         
         return $this->json([
             'message' => 'Hi',
