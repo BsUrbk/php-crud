f5ed84ef03edf5ec0b7c23e18a9f42520f4a5641
BsUrbk <bartosz.urbk@gmail.com>
2022-09-05 17:24:21 +0200

    Add product entity

---
+++ src/Entity/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\Uuid;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'uuid')]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
